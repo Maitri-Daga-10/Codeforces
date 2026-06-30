9cc77db14cb909d82c00de59dfe684b5fe239b67
Maitri daga <maitri.daga.10@gmail.com>
2026-06-30 22:40:52 +0530

    Problem 263A: Beautiful Matrix

---
+++ Problemset/BeautifulMatrix_263A.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class BeautifulMatrix_263A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int row = 0;
+        int col = 0;
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j <= 5; j++){
+                int num = sc.nextInt();
+                if (num == 1){
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+        System.out.println(Math.abs(row - 3) + Math.abs(col - 3));
+    }
+}
