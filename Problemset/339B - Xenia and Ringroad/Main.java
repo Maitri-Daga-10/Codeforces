23596179c01f7f423453574b8002c24f3b58b4ca
Maitri daga <maitri.daga.10@gmail.com>
2026-07-30 22:47:22 +0530

    339B - Xenia and Ringroad

---
+++ Problemset/339B - Xenia and Ringroad/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int current = 1;
+        long time = 0;
+        for (int i = 0; i < m; i++){
+            int target = sc.nextInt();
+            if (target >= current){
+                time += target - current;
+            }
+            else{
+                time += (n - current) + target;
+            }
+            current = target;
+        }
+        System.out.println(time);
+    }
+}
