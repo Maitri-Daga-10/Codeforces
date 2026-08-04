fcb277f6b65cd3773fdd2a953c0d593be39fab80
Maitri daga <maitri.daga.10@gmail.com>
2026-08-04 23:55:19 +0530

    580A - Kefa and First Steps

---
+++ Problemset/580A - Kefa and First Steps/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+        }
+        int count = 1;
+        int max = 1;
+        for (int i = 1; i < n; i++){
+            if (a[i] >= a[i - 1]){
+                count++;
+            } 
+            else{
+                count = 1;
+            }
+            max = Math.max(max, count);
+        }
+        System.out.println(max);
+    }
+}
