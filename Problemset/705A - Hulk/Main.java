1122078acd6ef5d793a915163963aa4a2890f485
Maitri daga <maitri.daga.10@gmail.com>
2026-08-26 23:43:06 +0530

    705A - Hulk

---
+++ Problemset/705A - Hulk/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i <= n; i++){
+            if (i % 2 == 1){
+                result.append("I hate");
+            } 
+            else{
+                result.append("I love");
+            }
+            if (i == n){
+                result.append(" it");
+            } 
+            else{
+                result.append(" that ");
+            }
+        }
+        System.out.println(result);
+    }
+}
